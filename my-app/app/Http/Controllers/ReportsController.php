174125ee4f123ddcf0c1fa0d05c2e43152d04bd6
Gaurang Parekh <gaurang9rising@gmail.com>
2025-01-21 14:14:50 +0530

    reports created

---
+++ my-app/app/Http/Controllers/ReportsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    public function __construct() {
+        // Controller construct   
+    }
+}
